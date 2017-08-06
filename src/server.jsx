66dc99965f979bf2bce4bb24c4319665572ce011
happyahluwalia <happyahluwalia@gmail.com>
2017-08-05 20:11:31 -0400

    Added locale detection logic on the server

--- src/server.jsx
+++ src/server.jsx
@@ -2,6 +2,10 @@ import express from 'express';
 import React from 'react';
 import ReactDom from 'react-dom/server';
 import App from './components/App';
+import cookieParser from 'cookie-parser';
+import acceptLanguage from 'accept-language';
+
+acceptLanguage.languages(['en', 'es']);
 
 const assetUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:8050' : '/';
 
@@ -23,12 +27,25 @@ function renderHTML(componentHTML) {
 }
 
 const app = express();
+app.use(cookieParser());
+
+/*
+   Detect the language of the user by first looking at cookie
+   (if he is repeat visitor) if first time then detect from
+   header's accept-language parameter else 
+   english
+*/
+function detectLocale(req){
+  const cookieLocale = req.cookies.locale;
+  return acceptLangague.get(cookieLocale || req.headers['accept-language']) || 'en';
+}
 
 app.use((req, res) => {
+  const locale = detectLocale(req);
   const componentHTML = ReactDom.renderToString(<App />);
-
+  res.cookie('locale', locale, {maxAge: (new Date() * 0.001) + (365 * 24 * 3600) });
   return res.end(renderHTML(componentHTML));
-});
+})
 
 const PORT = process.env.PORT || 3001;
 
